9eee2319aef058bb32c21923f0aeb222756a4cba
emcrusta <emcrusta@gmail.com>
2021-05-15 21:53:05 -0600

    update mobile menu to slide out from side

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const hamburgerMenu = document.querySelector('.hamburger-container');
+const hamburgerButton = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
 
 let menuOpen = false;
 
 function toggleMenu() {
 	if (!menuOpen) {
+		hamburgerMenu.classList.add('open');
+		hamburgerButton.classList.add('open');
 		nav.classList.add('visible');
 		menuOpen = true;
 	} else {
+		hamburgerMenu.classList.add('open');
+		hamburgerButton.classList.remove('open');
 		nav.classList.remove('visible');
 		menuOpen = false;
 	}
